7bb22899a2ac998fe0dd06129a20fdfa4b355f14
DavidDGamble <daviddgamble@gmail.com>
2022-12-23 11:22:34 -0800

    Add delete() to ClientController and views.

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -13,10 +13,29 @@ namespace HairSalon.Controllers
       _db = db;
     }
 
+    [HttpGet("/client/{id}/delete")]
+    public ActionResult Delete(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+
     [HttpPost("client/{id}/create")]
     public ActionResult Create(int id, Client client)
     {
+      client.StylistId = id;
+      _db.Clients.Add(client);
+      _db.SaveChanges();
       return Redirect($"/stylist/details/{id}");
+    }
+
+    [HttpPost("/client/{id}/delete/confirm")]
+    public ActionResult DeleteConfirm(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      _db.Clients.Remove(thisClient);
+      _db.SaveChanges();
+      return Redirect($"/stylist/details/{thisClient.StylistId}");
     }
   }
 }
